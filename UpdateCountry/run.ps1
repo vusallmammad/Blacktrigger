3000b2ca3d8452bf0e6bcabdf2d00b8ae4867083
vusallmammad <vusallmammad@outlook.com>
2026-02-14 12:26:01 +0400

    Update run.ps1

--- UpdateCountry/run.ps1
+++ UpdateCountry/run.ps1
@@ -25,31 +25,55 @@ if ($country -notin $allowedCountries) {
 }
 
 try {
-    Connect-AzAccount -Identity | Out-Null
+    # Get token directly from Managed Identity - no Az modules needed
+    $tokenResponse = Invoke-RestMethod `
+        -Uri "$($env:IDENTITY_ENDPOINT)?resource=https://management.azure.com&api-version=2019-08-01" `
+        -Headers @{ "X-IDENTITY-HEADER" = $env:IDENTITY_HEADER } `
+        -Method GET
 
-    $params = @{
-        UserPrincipalName = $upn
-        Country           = $country
-    }
+    $accessToken = $tokenResponse.access_token
 
-    $job = Start-AzAutomationRunbook `
-        -AutomationAccountName "blacklone" `
-        -ResourceGroupName $env:AUTOMATION_RESOURCE_GROUP `
-        -Name "Blacktrigger" `
-        -Parameters $params `
-        -RunOn "Activedirectory"
+    # Call Azure Automation REST API directly
+    $subscriptionId    = $env:SUBSCRIPTION_ID
+    $resourceGroup     = $env:AUTOMATION_RESOURCE_GROUP
+    $automationAccount = "blacklone"
+    $runbookName       = "Blacktrigger"
+    $jobId             = [guid]::NewGuid().ToString()
+
+    $apiUrl = "https://management.azure.com/subscriptions/$subscriptionId/resourceGroups/$resourceGroup/providers/Microsoft.Automation/automationAccounts/$automationAccount/jobs/${jobId}?api-version=2023-11-01"
+
+    $jobBody = @{
+        properties = @{
+            runbook = @{
+                name = $runbookName
+            }
+            parameters = @{
+                UserPrincipalName = $upn
+                Country           = $country
+            }
+            runOn = "Activedirectory"
+        }
+    } | ConvertTo-Json -Depth 5
+
+    $result = Invoke-RestMethod -Uri $apiUrl `
+        -Method PUT `
+        -Headers @{
+            Authorization  = "Bearer $accessToken"
+            "Content-Type" = "application/json"
+        } `
+        -Body $jobBody
 
     Push-OutputBinding -Name Response -Value ([HttpResponseContext]@{
         StatusCode = [HttpStatusCode]::OK
         Body       = (@{
             message = "Country change request submitted for $upn to $country"
-            jobId   = $job.JobId.ToString()
+            jobId   = $result.properties.jobId
         } | ConvertTo-Json)
     })
 }
 catch {
     Push-OutputBinding -Name Response -Value ([HttpResponseContext]@{
         StatusCode = [HttpStatusCode]::InternalServerError
-        Body       = (@{ message = "Failed to trigger runbook: $_" } | ConvertTo-Json)
+        Body       = (@{ message = "Failed: $_" } | ConvertTo-Json)
     })
 }
